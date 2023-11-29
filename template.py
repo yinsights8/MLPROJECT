6ae23de1f33c0a62bd480a8db432d9a6e80bb199
“yinsights8” <yinsights8@gmail.com>
2023-11-29 14:16:21 +0530

    Project structure

---
+++ template.py
@@ -0,0 +1,45 @@
+import os 
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+project_name = "Mlproject"
+
+list_of_files = [
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/Components/__init__.py",
+    f"src/{project_name}/Components/data_ingestion.py",
+    f"src/{project_name}/Components/data_transformation.py",
+    f"src/{project_name}/Components/model_trainer.py",
+    f"src/{project_name}/Components/model_monitering.py",
+    f"src/{project_name}/pipelines/__init__.py",
+    f"src/{project_name}/pipelines/training_pipeline.py",
+    f"src/{project_name}/pipelines/prediction_pipeline.py",
+    f"src/{project_name}/exception.py",
+    f"src/{project_name}/logger.py",
+    f"src/{project_name}/utils.py",
+    "main.py",
+    "app.py",
+    "Dockerfile.py",
+    "requirements.txt",
+    "setup.py"
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    # print(filepath)
+    filedir, filename = os.path.split(filepath)
+    # print(filedir, filename)
+
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info("Created directory : {filedir} for the file : {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath,"w") as f:
+            pass
+        logging.info(f"Creating empty file: {filepath}")
+
+    else:
+        logging.info("{filename} already exists")
